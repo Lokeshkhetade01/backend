daae9ed652ddc9a1d7b8a7e55812d0199b401709
Pranaykhetade02 <pranaykhetade152@gmail.com>
2026-03-04 23:53:29 +0530

    connect to mongo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log("hello ")
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+dotenv.config({
+  path:"./env"
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// first approach
+// import express from "express"
+// const app = express()
+// (async()=>{
+//   try {
+//       await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+//       app.on("error",()=>{
+//         console.log("Error: ",error)
+//         throw error
+//       })
+//       app.listen(process.env.PORT,()=>{
+//         console.log("App is listening on variable port")
+//       })
+//   } catch (error) {
+//     console.error("Error: ",error)
+//     throw error
+//   }
+// })()
